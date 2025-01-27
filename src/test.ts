09150b686a9a1cc872e32cdf9d50a256152d2dfa
Neal <mcandrineal@gmail.com>
2025-01-27 15:59:27 +0300

    add library management features with TypeScript interfaces and classes

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,61 @@
-console.log("hello world!");
+interface Book {
+  title: string;
+  author: string;
+  year: number;
+  isAvailable: boolean;
+}
+
+interface DigitalBook extends Book {
+  format: "pdf" | "epub" | "mobi";
+  downloadLink: string;
+}
+
+type addBook = (book: Book) => void;
+
+const isBookAvailable = (book: Book) => book.isAvailable;
+
+const listBookByAuthor = (books: Book[], author: string) =>
+  books.filter((book) => book.author === author);
+
+interface Library {
+    name: string;
+    books: Book[];
+    addBook: addBook;
+    isBookAvailable: (book: Book) => boolean;
+    listBookByAuthor: (author: string) => Book[];
+}
+
+class PublicLibrary implements Library {
+  public books: Book[] = [];
+    constructor(public name: string) {
+        this.name = name;
+    }
+
+    addBook(book: Book) {
+        this.books.push(book);
+    }
+
+    isBookAvailable(book: Book) {
+        return book.isAvailable;
+    }
+
+    listBookByAuthor(author: string) {
+        return this.books.filter((book) => book.author === author);
+    }
+}
+
+const addBook: addBook = (book) => {
+  console.log(book);
+};
+
+let book1: DigitalBook = {
+  title: "The Pragmatic Programmer",
+  author: "Andrew Hunt",
+  year: 2000,
+  isAvailable: true,
+  format: "pdf",
+  downloadLink:
+    "https://www.amazon.com/The-Pragmatic-Programmer-Journeyman-Master/dp/020161622X",
+};
+
+addBook(book1);
